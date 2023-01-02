a3bfc88e1d8bcdcb2c90c941163f2b69230fb615
ANURAG-R-NAIK <111006990+ANURAG-R-NAIK@users.noreply.github.com>
2023-01-02 22:09:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font,colorchooser,filedialog,messagebox
+import os
+
+
+main_appl = tk.Tk()
+main_appl.geometry("800x600")
+main_appl.title("ANU NOTEPAD")
+
+
+main_menu = tk.Menu()
+
+
+main_appl.mainloop()
